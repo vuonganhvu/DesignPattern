5c7f2aacf7b3780ebd00839b27177dfa6d023dcf
anhvv <anhvuong.neu@outlook.com>
2018-06-27 01:00:46 +0700

    convert module design pattern

---
+++ singleton/src/main/java/com/higgsup/designpattern/SingletonLazyMultithreaded.java
@@ -0,0 +1,23 @@
+package com.higgsup.designpattern;
+
+public class SingletonLazyMultithreaded {
+
+    private static SingletonLazyMultithreaded sc = null;
+    private String name;
+
+    private SingletonLazyMultithreaded() {
+        this.name = "SingletonLazyMultithreaded";
+    }
+
+    public static synchronized SingletonLazyMultithreaded getInstance() {
+        if (sc == null) {
+            sc = new SingletonLazyMultithreaded();
+        }
+        return sc;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
---
+++ singleton/src/main/java/com/higgsup/designpattern/SingletoneEnum.java
@@ -0,0 +1,8 @@
+package com.higgsup.designpattern;
+
+public class SingletoneEnum {
+
+    public enum SingleEnum {
+        SINGLETON_ENUM;
+    }
+}
